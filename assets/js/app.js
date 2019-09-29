c7e546455594db06926ae88c3d07123fd720549c
Treudey <alextreude@hotmail.com>
2019-09-29 17:43:24 -0400

    made code more object-oriented

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// a single state object to keep track of all the values
 const state = {
   float: 0,
   cashOnHand: 0,
@@ -10,6 +11,7 @@ const state = {
   get netTips() { return this.cashTips + this.dcTips - this.tipout }
 }
 
+// Store all the DOM IDs in a single object
 const domIDs = {
   float: 'float',
   floatSubtract: 'floatSubtract',
@@ -23,78 +25,116 @@ const domIDs = {
   tips: 'tips',
 };
 
-
-// Create custum event
+// Created custum event
 const valueChangeEvent = new CustomEvent("valueChange");
 
-const floatElement = document.getElementById(domIDs.float);
-const floatSubtractElement = document.getElementById(domIDs.floatSubtract);
-const floatHandler = (event) => {
-  state.float = parseFloat(floatElement.value);
-  floatSubtractElement.innerText = state.float;
-  cohElement.dispatchEvent(valueChangeEvent);
-};
-floatElement.addEventListener('keyup', floatHandler);
-floatElement.addEventListener('click', floatHandler);
+// A class to define each html elelment that's going to be manipulated
+class Field {
+  elementsToTrigger = [];
+  constructor(domStr, stateKey, type, elementsToTrigger, eventsListeningFor) {
+    this.type = type;
+    this.domStr = domStr;
+    this.stateKey = stateKey;
+    this.element = document.getElementById(domStr);
+    if (this.type === 'input') {
+      this.elementsToTrigger = elementsToTrigger;
+      this.eventsListeningFor = eventsListeningFor;
+    } else if (this.type === 'text') {
+      this.elementsToTrigger = [];
+      this.eventsListeningFor = ['valueChange'];
+    }
+  }
+  
+  eventHandler = () => {
+    if (this.type === 'input') {
+      state[this.stateKey] = this.element.value !== '' ? parseFloat(this.element.value) : 0;
+      console.log(state[this.stateKey]);
+      this.elementsToTrigger.forEach(el => {
+        setTimeout(() => { document.getElementById(el).dispatchEvent(valueChangeEvent) });
+      });
+    } else if (this.type === 'text') {
+      this.element.innerText = state[this.stateKey].toFixed(2);
+    }
+  }
 
-const cohElement = document.getElementById(domIDs.cashOnHand);
-const cohHandler = () => {
-  state.cashOnHand = parseFloat(cohElement.value);
-  setTimeout(() => { cashTotalElement.dispatchEvent(valueChangeEvent) });
-  setTimeout(() => { cashSalesElement.dispatchEvent(valueChangeEvent) });
-};
-cohElement.addEventListener('keyup', cohHandler);
-cohElement.addEventListener('click', cohHandler);
-cohElement.addEventListener('valueChange', cohHandler);
+  setupEventListeners() {
+    console.log(this.elementsToTrigger);
+    this.eventsListeningFor.forEach(event => {
+      this.element.addEventListener(event, this.eventHandler);
+    });
+  }
+}
 
-const cashTotalElement = document.getElementById(domIDs.cashTotal);
-cashTotalElement.addEventListener('valueChange', (event) => {
-  cashTotalElement.innerText = state.cashOnHandTotal.toFixed(2);
-});
-
-const cashSalesElement = document.getElementById(domIDs.cashSales);
-const cashSalesHandler = () => { 
-  state.cashSales = parseFloat(cashSalesElement.value);
-  setTimeout(() => { cashTipsElement.dispatchEvent(valueChangeEvent) });
-  setTimeout(() => { owedElement.dispatchEvent(valueChangeEvent) });
-  setTimeout(() => { tipsElement.dispatchEvent(valueChangeEvent) });
-};
-cashSalesElement.addEventListener('keyup', cashSalesHandler);
-cashSalesElement.addEventListener('click', cashSalesHandler);
-cashSalesElement.addEventListener('valueChange', cashSalesHandler);
-
-const cashTipsElement = document.getElementById(domIDs.cashTips);
-cashTipsElement.addEventListener('valueChange', (event) => {
-  cashTipsElement.innerText = state.cashTips.toFixed(2);
-});
-
-const dcTipsElement = document.getElementById(domIDs.dcTips);
-const dcTipsHandler = () => {
-  state.dcTips = parseFloat(dcTipsElement.value);
-  console.log('dc tips' + state.dcTips);
-  setTimeout(() => { tipsElement.dispatchEvent(valueChangeEvent) });
-  setTimeout(() => { owedElement.dispatchEvent(valueChangeEvent) });
-};
-dcTipsElement.addEventListener('keyup', dcTipsHandler);
-dcTipsElement.addEventListener('click', dcTipsHandler);
-
-const tipoutElement = document.getElementById(domIDs.tipout);
-const tipoutHandler = () => {
-  state.tipout = parseFloat(tipoutElement.value);
-  console.log('tipout' + state.tipout);
-  setTimeout(() => { tipsElement.dispatchEvent(valueChangeEvent) });
-  setTimeout(() => { owedElement.dispatchEvent(valueChangeEvent) });
-};
-tipoutElement.addEventListener('keyup', tipoutHandler);
-tipoutElement.addEventListener('click', tipoutHandler);
-
-const owedElement = document.getElementById(domIDs.owed);
-owedElement.addEventListener('valueChange', (event) => {
-  owedElement.innerText = state.owed.toFixed(2);
-});
-
-const tipsElement = document.getElementById(domIDs.tips);
-tipsElement.addEventListener('valueChange', (event) => {
-  console.log('net tips' + state.netTips)
-  tipsElement.innerText = state.netTips.toFixed(2);
-});
+const fieldArray = [
+  new Field(
+    domIDs.float, 
+    domIDs.float, 
+    'input', 
+    [domIDs.cashOnHand, domIDs.floatSubtract], 
+    ['keyup', 'click']
+  ),
+  new Field(
+    domIDs.floatSubtract, 
+    domIDs.float, 
+    'text', 
+    [], 
+    ['valueChange']
+  ),
+  new Field(
+    domIDs.cashOnHand,
+    domIDs.cashOnHand,
+    'input',
+    [domIDs.cashTotal, domIDs.cashSales],
+    ['keyup', 'click', 'valueChange']
+  ),
+  new Field(
+    domIDs.cashTotal,
+    'cashOnHandTotal',
+    'text',
+    [],
+    ['valueChange']
+  ),
+  new Field(
+    domIDs.cashSales,
+    domIDs.cashSales,
+    'input',
+    [domIDs.cashTips, domIDs.owed, domIDs.tips],
+    ['keyup', 'click', 'valueChange']
+  ),
+  new Field(
+    domIDs.cashTips,
+    domIDs.cashTips,
+    'text',
+    [],
+    ['valueChange']
+  ),
+  new Field(
+    domIDs.dcTips,
+    domIDs.dcTips,
+    'input',
+    [domIDs.tips, domIDs.owed],
+    ['keyup', 'click']
+  ),
+  new Field(
+    domIDs.tipout,
+    domIDs.tipout,
+    'input',
+    [domIDs.tips, domIDs.owed],
+    ['keyup', 'click']
+  ),
+  new Field(
+    domIDs.owed,
+    domIDs.owed,
+    'text',
+    [],
+    ['valueChange']
+  ),
+  new Field(
+    domIDs.tips,
+    'netTips',
+    'text',
+    [],
+    ['valueChange']
+  )
+];
+fieldArray.forEach(field => field.setupEventListeners());
